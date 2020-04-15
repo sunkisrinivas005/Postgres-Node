acceb5ef49584ac96384db01f6847b4b8f935755
srinivas01 <sunkisrinivas005@gmail.com>
2020-04-15 14:17:04 +0530

    added rabbitmq

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express')
-const bodyParser = require('body-parser')
+const bodyParser = require('body-parser');
+var amqp = require('amqplib/callback_api');
 const app = express();
 const { Pool, Client } = require('pg');
 const BasicRoutes = require('./Routes/index');
@@ -11,7 +12,27 @@ app.use(
   })
 )
 app.use('/routes', BasicRoutes);
-
+amqp.connect('amqp://localhost', function(error0, connection) {
+  if(error0){
+    console.log("error gotacha....")
+  }else {
+    console.log("successfully connected...")
+    connection.createChannel(function(error1, channel) {
+      if (error1) {
+        throw error1;
+      }
+      var queue = 'hello';
+      var msg = 'Hello world';
+  
+      channel.assertQueue(queue, {
+        durable: false
+      });
+  
+      channel.sendToQueue(queue, Buffer.from(msg));
+      console.log(" [x] Sent %s", msg);
+    });
+  }
+});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
