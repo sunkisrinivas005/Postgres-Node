987924a9399e0ab72ea8d370eafd7018a711a6f5
Srinivas Sunki <Srinivas.Sunki@flexoffers.com>
2021-01-25 21:01:40 +0530

    initial commit for aiThinkers

--- server.js
+++ server.js
@@ -1,38 +1,20 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-var amqp = require('amqplib/callback_api');
+var cors = require('cors');
 const app = express();
-const { Pool, Client } = require('pg');
 const BasicRoutes = require('./Routes/index');
-const port = 3000;
-app.use(express.json())
+const port = 5000;
+
+app.use(express.json());
+app.use(cors())
 app.use(
   bodyParser.urlencoded({
     extended: true,
   })
 )
+
+
 app.use('/routes', BasicRoutes);
-amqp.connect('amqp://localhost', function(error0, connection) {
-  if(error0){
-    console.log("error gotacha....")
-  }else {
-    console.log("successfully connected...")
-    connection.createChannel(function(error1, channel) {
-      if (error1) {
-        throw error1;
-      }
-      var queue = 'hello';
-      var msg = 'Hello world';
-  
-      channel.assertQueue(queue, {
-        durable: false
-      });
-  
-      channel.sendToQueue(queue, Buffer.from(msg));
-      console.log(" [x] Sent %s", msg);
-    });
-  }
-});
 
 app.listen(port, () => {
   console.log(`App running on port ${port}.`)
